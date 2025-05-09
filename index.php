39934f90b4050af34ee5b9091ea690b05aa36f4d
devluth <devturd@gmail.com>
2025-05-10 00:20:33 +0800

    feat (landing page): added button, target references and hero text

--- index.php
+++ index.php
@@ -4,18 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/example.css">
 </head>
 <body>
     <div id="wrapper">
         <div id="header">
             <div class="buttons">
-                <a href="" target="_blank">Page 1</a>
-                <a href="" target="_blank">Page 2</a>
-                </div>
+                <a href="" target="_self" class="btn">Home</a>
+                <a href="page/index.php" target="_self" class="btn">Basic PHP</a>
+            </div>
         </div>
         <div id="main">
             <div id="hero-text">
-                <h1>Welcome</h1>
+                <h1>Welcome Programmer!</h1>
+                <p>Discover the basics of PHP!</p>
+                <a href="page/index.php" class="btn"> PHP BASICS </a>
             </div>
             
         </div>
