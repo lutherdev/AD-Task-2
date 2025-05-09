c05e78a0e588f4a7d4b2b9ee68f8857de260fae3
Luther Dean <devturd@gmail.com>
2025-05-09 22:03:11 +0800

    feat (landing page): added div classes for layout

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <div class="buttons">
+                <a href="" target="_blank">Page 1</a>
+                <a href="" target="_blank">Page 2</a>
+                </div>
+        </div>
+        <div id="main">
+            <div id="hero-text">
+                <h1>Welcome</h1>
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>
