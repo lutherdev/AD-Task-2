e7f675fd6e3cda25a8849beadd5719288bd32af0
devluth <devturd@gmail.com>
2025-05-10 14:23:12 +0800

    feat (tutorial page): added a js for the buttons

---
+++ page/assets/js/example.js
@@ -0,0 +1,9 @@
+
+    function showOutputBox1(){
+        document.getElementById("outputBox1").style.display = "block";
+        console.log("ITS CLICKING");
+    }
+
+    function showOutputBox2(){
+        document.getElementById("outputBox2").style.display = "block";
+    }
