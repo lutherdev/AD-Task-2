c2d7c272d89a018c9b823a9d0fa18ada5b1f71ff
lutherdev <devturd@gmail.com>
2025-05-12 11:56:52 +0800

    refactor (tutorial page): replace header with navbar

--- page/index.php
+++ page/index.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <div id="wrapper">
-        <div id="header">
+        <div id="navbar">
             <div class="sitename">LEARNPHP</div>
             <div class="buttons">
                 <a href="../index.php" target="_self" class="btn">Home</a>
