db1ea502248aa50bf5e25411ec78a860ef2738da
lutherdev <devturd@gmail.com>
2025-05-12 21:21:10 +0800

    feat (tutorial page): added js for output button

--- page/tutorial-page/assets/js/example.js
+++ page/tutorial-page/assets/js/example.js
@@ -1,4 +1,9 @@
 
+    function showOutputBox0(){
+        document.getElementById("outputBox0").classList.toggle("visible");
+        console.log("ITS CLICKING");
+    }
+    
     function showOutputBox1(){
         document.getElementById("outputBox1").classList.toggle("visible");
         console.log("ITS CLICKING");
