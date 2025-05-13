4a03058d8a071f0f742de95424d0d22c7dc8c80d
lutherdev <devturd@gmail.com>
2025-05-13 20:47:04 +0800

    refactor (home page): replaced html with head and nav reusable components

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LEARNPHP</title>
-    <link rel="stylesheet" href="assets/css/example.css">
-</head>
-<body>
-    <div id="wrapper">
-        <div id="navbar">
+<?php
+define('BASE_PATH', __DIR__);
+require_once BASE_PATH . '/handlers/pageData.handler.php';
+require_once BASE_PATH . '/components/templates/head.component.php';
+require_once BASE_PATH . '/components/templates/nav.component.php';
+?>
+
+<?php
+head("Home");
+navHeader($navList);
+?>
+<!-- REMOVE FOR CHECKING
+        <div id="navbar">           
             <div class="sitename">LEARNPHP</div>
             <div class="buttons">
                 <a href="" target="_self" class="btn">Home</a>
                 <a href="page/tutorial-page/index.php" target="_self" class="btn">Basic PHP</a>
             </div>
-        </div>
+        </div> -->
         <div id="main">
             <div id="hero-text">
                 <h1>Welcome Programmer!</h1>
@@ -23,6 +25,5 @@
             </div>
             
         </div>
-    </div>
 </body>
 </html>
