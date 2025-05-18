4367e2de30d8e38853c44ee7e078cc370742e202
lutherdev <devturd@gmail.com>
2025-05-18 17:20:44 +0800

    refactor (tutorial page 2): used a component template in replace to manual output

--- page/tutorial2-page/index.php
+++ page/tutorial2-page/index.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ . '/../../utils/capitalizeFLetter.utils.php';
 require_once __DIR__ . '/../../handlers/pageData.handler.php';
-require_once __DIR__ . '/../../components/templates/head.component.php';
-require_once __DIR__ . '/../../components/templates/nav.component.php';
+require_once __DIR__ . '/../../components/head.component.php';
+require_once __DIR__ . '/../../components/nav.component.php';
 ?>
 
 <?php
@@ -16,22 +16,15 @@ navHeader($navList);
                     <p>You can think of functions like a tv remote, where each click of the button it does the expected thing you want</p>
                     <h5>Scenario: We want to see the lists of students' names in our database but CAPITALIZE</h5>
                 </div>
-                <div class="codebox">
-                    <h4>Code:</h4>
-                    <h5>$arrayStuds = ["Luther sambeli", "Ford Dean", "dean man", "lelouch britania"];<br>
-                    foreach ($arrayStuds as $studs):<br>
-                        echo "Output: " . ucwords($studs);<br>
-                    endforeach;
-                    "
-                </h5>
-                <button onclick="showOutputBox0()">Show Output</button>
-                    <div id="outputBox0" >
-                        <?php
-                        //TODO: Put an output here
-                        capitalizeFLetter($arrayStuds);
-                        ?>
-                    </div>
-                </div>
+                <?php
+                    $snippet = $codeBoxData[0]["snippet"];
+                    $funcName = $codeBoxData[0]["funcName"];
+                    $num = 0;
+                    ob_start();
+                    capitalizeFLetter($arrayStuds);
+                    $outputHtml = ob_get_clean();
+                    include __DIR__ . '/../../components/templates/codeBox.component.php';
+                ?>
             </div>
             <a href="../tutorial-page/index.php">BACK</a>
         </div>
