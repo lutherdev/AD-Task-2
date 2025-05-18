0097e749c3e8e419bcbb8016beb87f6d1c1d815c
lutherdev <devturd@gmail.com>
2025-05-18 13:34:25 +0800

    chore: cleaned and removed the comments

--- handlers/pageData.handler.php
+++ handlers/pageData.handler.php
@@ -1,35 +1,14 @@
 <?php
 $navList = [
-    ["label" => "Home", "url" => "/"],
-    ["label" => "Tutorial", "url" => "/page/tutorial-page/index.php"],
+    [
+        "label" => "Home", 
+        "url" => "/"
+    ],
+    [
+        "label" => "Tutorial", 
+        "url" => "/page/tutorial-page/index.php"
+    ],
 ];
 
 $arrayStuds = ["Luther sambeli", "Ford Dean", "dean man", "lelouch britania"];
-
-/*
-$productList = [
-    [
-        "productName" => "Apple MacBook Pro 17",
-        "details" => [
-            "color" => ["Silver", "Black"],
-            "category" => "Laptop",
-            "price" => 2999,
-        ],
-    ],
-    [
-        "productName" => "Microsoft Surface Pro",
-        "details" => [
-            "color" => ["White", "Black"],
-            "category" => "Laptop",
-            "price" => 1999,
-        ],
-    ],
-];
-$accounts = array(
-    "https://facebook.com",
-    "https://instagram.com",
-    "https://gmail.com",
-
-);
-*/
 ?>
