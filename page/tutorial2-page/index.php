60498d84bd83f9b81673bd214545bb7f67dff210
lutherdev <devturd@gmail.com>
2025-05-18 13:59:24 +0800

    chore (tutorial page 2): changed text in codebox

--- page/tutorial2-page/index.php
+++ page/tutorial2-page/index.php
@@ -12,15 +12,16 @@ navHeader($navList);
         <div id="main">
             <div class="box">
                 <div class="textbox">
-                    <h1>CLASSES AND FUNCTIONS</h1>
+                    <h1>FUNCTIONS</h1>
                     <p>You can think of functions like a tv remote, where each click of the button it does the expected thing you want</p>
                     <h5>Scenario: We want to see the lists of students' names in our database but CAPITALIZE</h5>
                 </div>
                 <div class="codebox">
                     <h4>Code:</h4>
-                    <h5>$arrayStuds = ["Luther", "Ford", "dean", "lelouch"]<br>
-                    for ($studs as $arrayStuds):<br>
-                        echo ucwords($studs);
+                    <h5>$arrayStuds = ["Luther sambeli", "Ford Dean", "dean man", "lelouch britania"];<br>
+                    foreach ($arrayStuds as $studs):<br>
+                        echo "Output: " . ucwords($studs);<br>
+                    endforeach;
                     "
                 </h5>
                 <button onclick="showOutputBox0()">Show Output</button>
